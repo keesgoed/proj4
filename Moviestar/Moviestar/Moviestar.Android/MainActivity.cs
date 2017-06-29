cf9ca2022a028942465ca5ddd2afa47bf146c7a4
Kees <keesgoed@hotmail.com>
2017-06-29 12:10:04 +0200

    spinner (unfinished) + movie page

--- Moviestar/Moviestar/Moviestar.Android/MainActivity.cs
+++ Moviestar/Moviestar/Moviestar.Android/MainActivity.cs
@@ -9,27 +9,90 @@ using Android.OS;
 
 namespace Moviestar.Droid
 {
-	[Activity (Label = "Moviestar.Android", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+    [Activity(Label = "Moviestar.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
 
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+        String selectedItem = "Recommended movies";
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+            // Set our view from the "main" layout resource
+            //SetContentView (Resource.Layout.Login);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
-		}
-	}
+            SetContentView(Resource.Layout.Main);
+            update1();
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+
+            //create spinner
+
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.spinner);
+            spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            var adapter = ArrayAdapter.CreateFromResource(this, Resource.Array.menu_array, Android.Resource.Layout.SimpleSpinnerItem);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner.Adapter = adapter;
+
+        }
+
+        public void update1()
+        {
+            Button MoviePageButton = FindViewById<Button>(Resource.Id.moviePageButton);
+            MoviePageButton.Click += MoviePageButton_Click;
+
+        }
+
+        public void update2()
+        {
+            Button MoviePageBackButton = FindViewById<Button>(Resource.Id.moviePageBackButton);
+            MoviePageBackButton.Click += MoviePageBackButton_Click;
+        }
+
+        private void MoviePageButton_Click(object sender, EventArgs e)
+        {
+            SetContentView(Resource.Layout.MoviePage);
+            update2();
+        }
+
+        private void MoviePageBackButton_Click(object sender, EventArgs e)
+        {
+            SetContentView(Resource.Layout.Main);
+            update1();
+        }
+		
+        public void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner = (Spinner)sender;
+            Console.WriteLine(this.selectedItem);
+            if (Convert.ToString(spinner.GetItemAtPosition(e.Position)) != this.selectedItem)
+            {
+                changeView();
+                Console.WriteLine("Writeline in the if statement " + this.selectedItem);
+                this.selectedItem = Convert.ToString(spinner.GetItemAtPosition(e.Position));
+            }
+            Console.WriteLine(this.selectedItem);
+        }
+
+        private void changeView()
+        {
+
+            if (this.selectedItem == "Search")
+            {
+                // SetContentView(Resource.Layout.Search);
+            }
+            if (this.selectedItem == "Recommended movies")
+            {
+                SetContentView(Resource.Layout.Main);
+            }
+            if (this.selectedItem == "Login")
+            {
+                //  SetContentView(Resource.Layout.Login);
+            }
+        }
+    }
 }
+
 
 
--- Moviestar/Moviestar/Moviestar.Android/Resources/Resource.designer.cs
+++ Moviestar/Moviestar/Moviestar.Android/Resources/Resource.designer.cs
@@ -28,6 +28,22 @@ namespace Moviestar.Droid
 		{
 		}
 		
+		public partial class Array
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int menu_array = 2130968576;
+			
+			static Array()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Array()
+			{
+			}
+		}
+		
 		public partial class Attribute
 		{
 			
@@ -45,7 +61,13 @@ namespace Moviestar.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int cover = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int star = 2130837506;
 			
 			static Drawable()
 			{
@@ -60,8 +82,95 @@ namespace Moviestar.Droid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int ScrollLayout = 2131034113;
+			
+			// aapt resource value: 0x7f05000b
+			public const int containerMoviePage = 2131034123;
+			
+			// aapt resource value: 0x7f050019
+			public const int coverMoviePage = 2131034137;
+			
+			// aapt resource value: 0x7f05001c
+			public const int descriptionMoviePage = 2131034140;
+			
+			// aapt resource value: 0x7f050015
+			public const int genreMoviePage = 2131034133;
+			
+			// aapt resource value: 0x7f050008
+			public const int headerLayout = 2131034120;
+			
+			// aapt resource value: 0x7f050005
+			public const int imageView1 = 2131034117;
+			
+			// aapt resource value: 0x7f05001d
+			public const int imageView2 = 2131034141;
+			
+			// aapt resource value: 0x7f05001b
+			public const int lblActor = 2131034139;
+			
+			// aapt resource value: 0x7f05001a
+			public const int lblDirector = 2131034138;
+			
+			// aapt resource value: 0x7f050017
+			public const int lblDuration = 2131034135;
+			
+			// aapt resource value: 0x7f050018
+			public const int lblRating = 2131034136;
+			
+			// aapt resource value: 0x7f050016
+			public const int linearLayout1 = 2131034134;
+			
+			// aapt resource value: 0x7f05000f
+			public const int linearLayout2 = 2131034127;
+			
+			// aapt resource value: 0x7f05000c
+			public const int linearLayout3 = 2131034124;
+			
+			// aapt resource value: 0x7f050002
+			public const int linearMovieContainer = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int movieBlockCover = 2131034116;
+			
+			// aapt resource value: 0x7f05000e
+			public const int moviePageBackButton = 2131034126;
+			
+			// aapt resource value: 0x7f050007
+			public const int moviePageButton = 2131034119;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int relativeLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f05000a
+			public const int searchBar = 2131034122;
+			
+			// aapt resource value: 0x7f050009
+			public const int spinner = 2131034121;
+			
+			// aapt resource value: 0x7f050010
+			public const int star1MoviePage = 2131034128;
+			
+			// aapt resource value: 0x7f050011
+			public const int star2MoviePage = 2131034129;
+			
+			// aapt resource value: 0x7f050012
+			public const int star3MoviePage = 2131034130;
+			
+			// aapt resource value: 0x7f050013
+			public const int star4MoviePage = 2131034131;
+			
+			// aapt resource value: 0x7f050014
+			public const int star5MoviePage = 2131034132;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView1 = 2131034115;
+			
+			// aapt resource value: 0x7f050006
+			public const int textView2 = 2131034118;
+			
+			// aapt resource value: 0x7f05000d
+			public const int titleMoviePage = 2131034125;
 			
 			static Id()
 			{
@@ -79,31 +188,15 @@ namespace Moviestar.Droid
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
 			
+			// aapt resource value: 0x7f030001
+			public const int MoviePage = 2130903041;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 			}
 			
 			private Layout()
-			{
-			}
-		}
-		
-		public partial class String
-		{
-			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
-			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
-			
-			static String()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private String()
 			{
 			}
 		}
