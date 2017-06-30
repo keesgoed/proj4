8aa315d540ea33e698c0f6fc8d4699023c36b680
Kees Goedegebuure <keesgoed@hotmail.com>
2017-06-30 02:51:37 +0200

    add birkan page

--- Moviestar/Moviestar/Moviestar.Android/MoviePage.cs
+++ Moviestar/Moviestar/Moviestar.Android/MoviePage.cs
@@ -17,6 +17,7 @@ namespace Moviestar.Droid
     {
         String selected_item;
         String movie_ID;
+        int rating;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -25,16 +26,78 @@ namespace Moviestar.Droid
 
             movie_ID = Intent.GetStringExtra("MovieID") ?? "Data not available";
 
-            ImageButton starbutton1 = FindViewById<ImageButton>(Resource.Id.star1MoviePage);
-            ImageButton starbutton2 = FindViewById<ImageButton>(Resource.Id.star2MoviePage);
-            ImageButton starbutton3 = FindViewById<ImageButton>(Resource.Id.star3MoviePage);
-            ImageButton starbutton4 = FindViewById<ImageButton>(Resource.Id.star4MoviePage);
-            ImageButton starbutton5 = FindViewById<ImageButton>(Resource.Id.star5MoviePage);
-            starbutton1.Click += SaveRating;
-            starbutton2.Click += SaveRating;
-            starbutton3.Click += SaveRating;
-            starbutton4.Click += SaveRating;
-            starbutton5.Click += SaveRating;
+            page1.update();
+        }
+
+   public void update()
+        {
+            var rate1 = FindViewById<ImageButton>(Resource.Id.rate1);
+            var rate2 = FindViewById<ImageButton>(Resource.Id.rate2);
+            var rate3 = FindViewById<ImageButton>(Resource.Id.rate3);
+            var rate4 = FindViewById<ImageButton>(Resource.Id.rate4);
+            var rate5 = FindViewById<ImageButton>(Resource.Id.rate5);
+
+            //Resets the buttons
+            void ResetButton()
+            {
+                rate1.SetImageResource(Resource.Drawable.emptystar);
+                rate2.SetImageResource(Resource.Drawable.emptystar);
+                rate3.SetImageResource(Resource.Drawable.emptystar);
+                rate4.SetImageResource(Resource.Drawable.emptystar);
+                rate5.SetImageResource(Resource.Drawable.emptystar);
+            }
+
+            //Each button has to add a rate to a movie and add it to the database
+            rate1.Touch += (object sender, View.TouchEventArgs e) => {
+                ResetButton();
+                if (e.Event.Action == MotionEventActions.Down || e.Event.Action == MotionEventActions.Up)
+                {
+                    rate1.SetImageResource(Resource.Drawable.fullstar);
+                    rating = 1;
+                }
+            };
+            rate2.Touch += (object sender, View.TouchEventArgs e) => {
+                ResetButton();
+                if (e.Event.Action == MotionEventActions.Down || e.Event.Action == MotionEventActions.Up)
+                {
+                    rate1.SetImageResource(Resource.Drawable.fullstar);
+                    rate2.SetImageResource(Resource.Drawable.fullstar);
+                    rating = 2;
+                }
+            };
+            rate3.Touch += (object sender, View.TouchEventArgs e) => {
+                ResetButton();
+                if (e.Event.Action == MotionEventActions.Down || e.Event.Action == MotionEventActions.Up)
+                {
+                    rate1.SetImageResource(Resource.Drawable.fullstar);
+                    rate2.SetImageResource(Resource.Drawable.fullstar);
+                    rate3.SetImageResource(Resource.Drawable.fullstar);
+                    rating = 3;
+                }
+            };
+            rate4.Touch += (object sender, View.TouchEventArgs e) => {
+                ResetButton();
+                if (e.Event.Action == MotionEventActions.Down || e.Event.Action == MotionEventActions.Up)
+                {
+                    rate1.SetImageResource(Resource.Drawable.fullstar);
+                    rate2.SetImageResource(Resource.Drawable.fullstar);
+                    rate3.SetImageResource(Resource.Drawable.fullstar);
+                    rate4.SetImageResource(Resource.Drawable.fullstar);
+                    rating = 4;
+                }
+            };
+            rate5.Touch += (object sender, View.TouchEventArgs e) => {
+                ResetButton();
+                if (e.Event.Action == MotionEventActions.Down || e.Event.Action == MotionEventActions.Up)
+                {
+                    rate1.SetImageResource(Resource.Drawable.fullstar);
+                    rate2.SetImageResource(Resource.Drawable.fullstar);
+                    rate3.SetImageResource(Resource.Drawable.fullstar);
+                    rate4.SetImageResource(Resource.Drawable.fullstar);
+                    rate5.SetImageResource(Resource.Drawable.fullstar);
+                    rating = 5;
+                }
+            };
         }
 
         private void SaveRating(object sender, EventArgs e)
@@ -42,9 +105,17 @@ namespace Moviestar.Droid
             int id = ((ImageButton)sender).Id;
             String btnId = id.ToString();
 
-            TextView description = FindViewById<TextView>(Resource.Id.descriptionMoviePage);
+            TextView description = FindViewById<TextView>(Resource.Id.moviePageTitle);
             description.Text = movie_ID;
 
         }
+
+        private void UploadRating()
+        {
+
+
+        }
+
+        MoviePage page1 = new MoviePage();
     }
 }
