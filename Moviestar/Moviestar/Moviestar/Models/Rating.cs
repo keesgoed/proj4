c60e6d75ec6557935b6514db275e76704f45db0a
wouter <0934955@hr.nl>
2017-07-04 12:57:11 +0200

    Portable class added

---
+++ Moviestar/Moviestar/Moviestar/Models/Rating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moviestar.Models
+{
+    public class Rating
+    {
+        public string movie_id { get; set; }
+        public string user_id { get; set; }
+        public int rating { get; set; }
+    }
+}
