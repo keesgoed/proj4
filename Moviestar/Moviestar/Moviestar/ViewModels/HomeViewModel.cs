b1eb29dc2668fd07375490fc86e3f383eee7b560
wouter <0934955@hr.nl>
2017-07-04 21:09:24 +0200

    Rest API Works

---
+++ Moviestar/Moviestar/Moviestar/ViewModels/HomeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Moviestar.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moviestar.ViewModels
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel()
+        {
+
+        }
+
+        public List<Movie> GetMovies()
+        {
+            List<Movie> movielist = new List<Movie>();
+            var httpconnect = new Connection();
+            string jsonresponse = httpconnect.GetAPI();
+            movielist = JsonConvert.DeserializeObject<List<Movie>>(jsonresponse);
+            return movielist;
+        }
+    }
+}
