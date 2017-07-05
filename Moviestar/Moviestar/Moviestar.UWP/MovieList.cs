1bd6a885bfd8b1082984ab40b58bc89c825e7623
Kees Goedegebuure <keesgoed@hotmail.com>
2017-07-05 20:34:19 +0200

    Fix movieblock

--- Moviestar/Moviestar/Moviestar.UWP/MovieList.cs
+++ Moviestar/Moviestar/Moviestar.UWP/MovieList.cs
@@ -12,7 +12,6 @@ namespace Moviestar.UWP
     class MovieList
     {
 
-        public string Test { set; get; } = "test waarde";
         private List<Models.Movie> unfilteredMovie = new List<Models.Movie>();
 
         public List<Models.Movie> movie { get; set; }
@@ -25,9 +24,7 @@ namespace Moviestar.UWP
             HomeViewModel movies = new HomeViewModel();
             foreach (var moviedata in movies.GetMovies())
             {
-                Debug.WriteLine("Dit is een test printline " + moviedata.id);
                 movie.Add(new Models.Movie(moviedata.id, moviedata.actor_1_name, moviedata.actor_2_name, moviedata.actor_3_name, moviedata.genres, moviedata.title, moviedata.imdb_score));
-                Debug.WriteLine(movie[0].title);
             }
   
         }
