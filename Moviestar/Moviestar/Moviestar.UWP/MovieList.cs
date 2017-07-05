576fbf67e3ae2b32393aacad5d32655058909e86
CasperCX <0927578@hr.nl>
2017-07-05 12:50:40 +0200

    implemented commented-out adapter patten UWP

---
+++ Moviestar/Moviestar/Moviestar.UWP/MovieList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moviestar.Interfaces;
+
+namespace Moviestar.UWP
+{
+    class MovieList
+    {
+        //////// Create windows controls instance
+       // windowsControls windows_controls = new windowsControls();
+
+        //////// Set android Controls to work with the global adapter
+        //////// Not working for some reason in UWP version
+       // IControls AndroidAdapter = new windowsControlsAdapter(windows_controls);
+
+    }
+
+}
--- Moviestar/Moviestar/Moviestar.UWP/windowsControlsAdapter.cs
+++ Moviestar/Moviestar/Moviestar.UWP/windowsControlsAdapter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Moviestar.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Moviestar.UWP
 {
-    class windowsControlsAdapter // : IControls
+    class windowsControlsAdapter : IControls
     {
         windowsControls windowsControls;
 
